496a5c2f1b3174a4e7100fb7fc2739630fee31b4
AdamT3H <adamogorodnik@gmail.com>
2024-10-12 22:35:20 +0300

    lab3

--- LB3.cpp
+++ LB3.cpp
@@ -1,83 +1,45 @@
 #include <iostream>
-#include <algorithm>
-#include <string> 
-#include <string.h> 
+#include <cstring>
 
-/* ЗАВДАННЯ: вивисти список українських команд, та поміняти місцями Динамо та Шахар*/
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-/*
-[
-    "Динамо",
-    "Дніпро-1",
-    "Зоря",
-    "Ворскла",
-    "Олександрія",
-    "Колос",
-    "Металіст",
-    "Рух",
-    "ЛНЗ",
-    "Оболонь",
-    "Верес",
-    "Шахтар"
-];
-
-*/
-
-
-int main(){
+int main() {
     setlocale(LC_ALL, "Ukr");
     
-    int index_of_name1 = -111;
-    int index_of_name2 = -111; // чому я тут не можу використати extern, точніше як його тут коректно використати
-    
-    const char* name1 = "Динамо";
-    const char* name2 = "Шахтар";
-    
-    const char* list_of_all_clubs[] = {    
-        "Динамо",
-        "Шахтар",
-        "Дніпро-1",
-        "Зоря",
-        "Ворскла",
-        "Олександрія",
-        "Колос",
-        "Металіст",
-        "Рух",
-        "ЛНЗ",
-        "Оболонь",
-        "Верес",
-    };
-    
-    int size_of_list = sizeof(list_of_all_clubs) / sizeof(list_of_all_clubs[0]);
-    
-    std::cout<< "Лист номер один:" << '\n' << std::endl;
-    
-    for (int i = 0; i < size_of_list; i++){
-            std::cout<< list_of_all_clubs[i] << std::endl;
-            
-        if (strcmp(list_of_all_clubs[i], name1) == 0) {
-            index_of_name1 = i;
-        }
+    int index_shakhtar = -1; 
+    int index_dynamo = -1;
 
-        if (strcmp(list_of_all_clubs[i], name2) == 0) {
-            index_of_name2 = i;
-        }
-            
-    };
-    std::cout<< '\n' << std::endl;
-    
-    if (index_of_name1 != -111 && index_of_name2 != -111){
-        list_of_all_clubs[index_of_name1] = name2;
-        list_of_all_clubs[index_of_name2] = name1;
-    } else {
-        std::cout << "Один або обидва клуби не знайдено." << std::endl;
+    const char* teams[] = { "Динамо", "Шахтар", "Ворскла", "Карпати", "Зоря" };
+
+    const int SIZE = ARRAY_SIZE(teams);
+
+    std::cout << "Початковий список команд:\n";
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << teams[i] << std::endl;
     }
-    
-        std::cout<< "Лист номер два:" << '\n' << std::endl;
-    
-    for (int i = 0; i < size_of_list; i++){
-        std::cout<< list_of_all_clubs[i] << std::endl;
-    };
-    
+
+    for (int i = 0; i < SIZE; i++) {
+        if (std::strcmp(teams[i], "Шахтар") == 0) {
+            index_shakhtar = i;
+        }
+        if (std::strcmp(teams[i], "Динамо") == 0) {
+            index_dynamo = i;
+        }
+    }
+
+    if (index_shakhtar != -1 && index_dynamo != -1) {
+        const char* temp = teams[index_shakhtar];
+        teams[index_shakhtar] = teams[index_dynamo];
+        teams[index_dynamo] = temp;
+    } else {
+        std::cout << "Не знайдено одну або обидві команди." << std::endl;
+        return 1;
+    }
+
+    std::cout << "\nСписок після обміну:\n";
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << teams[i] << std::endl;
+    }
+
     return 0;
 }
