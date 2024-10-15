3b9be04fa5e1ea575e3cb8dca3a1bff7a794bba3
AdamT3H <adamogorodnik@gmail.com>
2024-10-15 15:24:03 +0300

    Lab5

--- LB5.cpp
+++ LB5.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-void outputMatr(int* matr, int n, int m) {
+void outputMatr(int** matr, int n, int m) {
     for (int i = 0; i < n; ++i, std::cout << std::endl) {
         for (int j = 0; j < m; ++j) {
-            std::cout << matr[i * m + j] << "\t";
+            std::cout << matr[i][j] << "\t";
         }
     }
     std::cout << std::endl;
 }
 
-void fillStaticMatr(int* matr, int n, int m) {
+void fillStaticMatr(int** matr, int n, int m) {
     int staticValues[3][3] = {
         {10, -2, 3},
         {2, -10, 9},
@@ -18,19 +18,19 @@ void fillStaticMatr(int* matr, int n, int m) {
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            matr[i * m + j] = staticValues[i][j];
+            matr[i][j] = staticValues[i][j];
         }
     }
 }
 
-int findMaxOfRowMins(int* matr, int n, int m) {
+int findMaxOfRowMins(int** matr, int n, int m) {
     int rowMins[n];
 
     for (int i = 0; i < n; ++i) {
-        int minVal = matr[i * m];
+        int minVal = matr[i][0];
         for (int j = 1; j < m; ++j) {
-            if (matr[i * m + j] < minVal) {
-                minVal = matr[i * m + j];
+            if (matr[i][j] < minVal) {
+                minVal = matr[i][j];
             }
         }
         rowMins[i] = minVal;
@@ -48,13 +48,17 @@ int findMaxOfRowMins(int* matr, int n, int m) {
 
 int main() {
     const int n = 3, m = 3;
-    int mas2d[n][m];
 
-    fillStaticMatr(&mas2d[0][0], n, m);
+    int** mas2d = new int*[n];
+    for (int i = 0; i < n; ++i) { //виділення памяті для цієї матриці
+        mas2d[i] = new int[m];
+    }
+
+    fillStaticMatr(mas2d, n, m);
     
-    outputMatr(&mas2d[0][0], n, m);
+    outputMatr(mas2d, n, m);
 
-    int maxOfMins = findMaxOfRowMins(&mas2d[0][0], n, m);
+    int maxOfMins = findMaxOfRowMins(mas2d, n, m);
     std::cout << "максимум з мінімумів = " << maxOfMins << std::endl;
 
     return 0;
