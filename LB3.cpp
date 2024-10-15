4686b0b83a0ef15e96021b2b8140aa91f705a209
AdamT3H <adamogorodnik@gmail.com>
2024-10-15 15:14:36 +0300

    lab3

--- LB3.cpp
+++ LB3.cpp
@@ -1,45 +1,21 @@
 #include <iostream>
-#include <cstring>
-
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+#include <string.h>
 
 int main() {
-    setlocale(LC_ALL, "Ukr");
-    
-    int index_shakhtar = -1; 
-    int index_dynamo = -1;
+    char teams[200] = "Динамо, Шахтар, Карпати, Зоря, Ворскла";
+    std::cout << "Рядок до заміни: " << teams << std::endl;
+    char shachtar[] = "Шахтар";
+    char dunamo[] = "Динамо";
+    char* find_shachtar = strstr(teams, shachtar);
+    char* find_dunamo = strstr(teams, dunamo);
 
-    const char* teams[] = { "Динамо", "Шахтар", "Ворскла", "Карпати", "Зоря" };
-
-    const int SIZE = ARRAY_SIZE(teams);
-
-    std::cout << "Початковий список команд:\n";
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << teams[i] << std::endl;
+    if (find_shachtar && find_dunamo) {
+        char temp[100];
+        strcpy(temp, shachtar);
+        strncpy(find_shachtar, dunamo, strlen(dunamo));
+        strncpy(find_dunamo, temp, strlen(temp));
     }
 
-    for (int i = 0; i < SIZE; i++) {
-        if (std::strcmp(teams[i], "Шахтар") == 0) {
-            index_shakhtar = i;
-        }
-        if (std::strcmp(teams[i], "Динамо") == 0) {
-            index_dynamo = i;
-        }
-    }
-
-    if (index_shakhtar != -1 && index_dynamo != -1) {
-        const char* temp = teams[index_shakhtar];
-        teams[index_shakhtar] = teams[index_dynamo];
-        teams[index_dynamo] = temp;
-    } else {
-        std::cout << "Не знайдено одну або обидві команди." << std::endl;
-        return 1;
-    }
-
-    std::cout << "\nСписок після обміну:\n";
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << teams[i] << std::endl;
-    }
-
+    std::cout << "Рядок після заміни: " << teams << std::endl;
     return 0;
 }
